3fb2ab2ad7d63fa856f1518f2738c46d40f66068
kerenhas <48164083+kerenhas@users.noreply.github.com>
2020-03-27 10:16:58 +0100

    commit 2

---
+++ src/Controller/viewController.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class viewController extends AbstractController
+
+{
+/**
+* @Route("/view",name="projet_view")
+
+*/
+public function view(){
+
+$clientCurl = new CurlHttpClient();
+$response=$clientCurl->request("POST", "http://concoursphoto.ort-france.fr/api/matrice",
+            ['headers' => ['Content-Type' => 'application/json'],'body' => '{}']);
+
+            $Content=$response->getContent();
+dd($Content);
+return new Response('ORT-CSI TP API ');
+					}
+}
+
+ ?>
